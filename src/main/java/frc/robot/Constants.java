84b089af418260a3a3dc04c8ae00a092dc99c475
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-16 22:05:07 -0700

    blank arm subsystem and constants, update vendordep

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public final class Constants {
+    public static final class Arm {
+        public static final int ROLLER_MOTOR_ID = 15;
+        public static final int ARM_MOTOR_ID = 16;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class ArmSubsystem extends SubsystemBase {
+    private TalonFX armMotor, rollerMotor;
+
+    public ArmSubsystem() {
+
+    }
+    
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+}
