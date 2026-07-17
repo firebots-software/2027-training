3f6210ddfc246b881cec4eb08ff2a6fc15b15e60
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-16 19:59:56 -0700

    fix project type

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,94 +4,101 @@
 
 package frc.robot;
 
-import edu.wpi.first.hal.DriverStationJNI;
-import edu.wpi.first.util.WPIUtilJNI;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.internal.DriverStationModeThread;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This class is run automatically. If you change the name of this class or the package after
- * creating this project, you must also update the Main.java file in the project.
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
  */
-public class Robot extends RobotBase {
-  public Robot() {}
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  public void disabled() {}
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
 
-  public void autonomous() {}
-
-  public void teleop() {}
-
-  public void test() {}
-
-  private volatile boolean m_exit;
-
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
   @Override
-  public void startCompetition() {
-    DriverStationModeThread modeThread = new DriverStationModeThread();
+  public void robotPeriodic() {}
 
-    int event = WPIUtilJNI.createEvent(false, false);
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
 
-    DriverStation.provideRefreshedDataEventHandle(event);
-
-    // Tell the DS that the robot is ready to be enabled
-    DriverStationJNI.observeUserProgramStarting();
-
-    while (!Thread.currentThread().isInterrupted() && !m_exit) {
-      if (isDisabled()) {
-        modeThread.inDisabled(true);
-        disabled();
-        modeThread.inDisabled(false);
-        while (isDisabled()) {
-          try {
-            WPIUtilJNI.waitForObject(event);
-          } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-          }
-        }
-      } else if (isAutonomous()) {
-        modeThread.inAutonomous(true);
-        autonomous();
-        modeThread.inAutonomous(false);
-        while (isAutonomousEnabled()) {
-          try {
-            WPIUtilJNI.waitForObject(event);
-          } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-          }
-        }
-      } else if (isTest()) {
-        modeThread.inTest(true);
-        test();
-        modeThread.inTest(false);
-        while (isTest() && isEnabled()) {
-          try {
-            WPIUtilJNI.waitForObject(event);
-          } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-          }
-        }
-      } else {
-        modeThread.inTeleop(true);
-        teleop();
-        modeThread.inTeleop(false);
-        while (isTeleopEnabled()) {
-          try {
-            WPIUtilJNI.waitForObject(event);
-          } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-          }
-        }
-      }
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
     }
-
-    DriverStation.removeRefreshedDataEventHandle(event);
-    modeThread.close();
   }
 
+  /** This function is called once when teleop is enabled. */
   @Override
-  public void endCompetition() {
-    m_exit = true;
-  }
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
