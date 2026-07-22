6058bb2fbea09e24a1e355c31dbbdf8632dee174
Aarav Agarwal <aaravsfo@gmail.com>
2026-07-22 12:38:50 -0700

    Finish HW 2 - ArmSubsystem Implementation

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,15 +5,36 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class ArmSubsystem extends SubsystemBase {
     private TalonFX armMotor, rollerMotor;
 
-    public ArmSubsystem() {
+    private final DutyCycleOut m_dutyCycleRequest = new DutyCycleOut(0.0);
+    private final VoltageOut m_voltageRequest = new VoltageOut(0.0);
+    private final TorqueCurrentFOC m_torqueRequest = new TorqueCurrentFOC(0.0);
 
+    public ArmSubsystem() {
+        armMotor = new TalonFX(Constants.Arm.ARM_MOTOR_ID);
+        rollerMotor = new TalonFX(Constants.Arm.ROLLER_MOTOR_ID);
     }
     
+    public void setRollerDutyCycle (double num) {
+        rollerMotor.setControl(m_dutyCycleRequest.withOutput(num));
+    }
+
+    public void setRollerVoltage (double num) {
+        rollerMotor.setControl(m_voltageRequest.withOutput(num));
+    }
+
+    public void setArmTorqueCurrent (double num) {
+        armMotor.setControl(m_torqueRequest.withOutput(num));
+    }
 
     @Override
     public void periodic() {
