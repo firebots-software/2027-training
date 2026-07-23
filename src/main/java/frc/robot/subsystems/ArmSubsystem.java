0776da4837e5cae9c3a0421274b983c6824cd751
Maryam <maryamshaikh320@gmail.com>
2026-07-22 17:51:01 -0700

    homework done

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,15 +5,35 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 
 public class ArmSubsystem extends SubsystemBase {
     private TalonFX armMotor, rollerMotor;
+    private final DutyCycleOut m_dutyCycleRequest = new DutyCycleOut(0.0);
+    private final VoltageOut m_voltageRequest = new VoltageOut(0.0);
+    private final TorqueCurrentFOC m_torqueRequest = new TorqueCurrentFOC(0.0);
+
 
     public ArmSubsystem() {
+        armMotor = new TalonFX(Constants.Arm.ARM_MOTOR_ID);
+        rollerMotor = new TalonFX(Constants.Arm.ROLLER_MOTOR_ID);
+    }
 
+    public void setRollerDutyCycle(double value) {
+        rollerMotor.setControl(m_dutyCycleRequest.withOutput(value));
+    }
+    public void setRollerVoltage(double value) {
+        rollerMotor.setControl(m_voltageRequest.withOutput(value));
     }
     
+    public void setArmTorque(double value) {
+        armMotor.setControl(m_torqueRequest.withOutput(value));
+    }
 
     @Override
     public void periodic() {
