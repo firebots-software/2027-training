4862cf4dc2204f53044262c2347059b6f643caec
aashnassharma <aashna.s.sharma@gmail.com>
2026-07-22 17:25:54 -0700

    homework 2

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,13 +5,37 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class ArmSubsystem extends SubsystemBase {
     private TalonFX armMotor, rollerMotor;
+    private DutyCycleOut m_dutyCycleRequest;
+    private VoltageOut m_voltageRequest;
+    private TorqueCurrentFOC m_torqueRequest;
+
+    public void setRollerDutyCycle(double x) {
+        rollerMotor.setControl(m_dutyCycleRequest.withOutput(x));
+    }
+
+    public void setRollerVoltage(double x) {
+        rollerMotor.setControl(m_voltageRequest.withOutput(x));
+    }
+
+    public void setArmTorqueCurrent(double x) {
+        armMotor.setControl(m_torqueRequest.withOutput(x));
+    }
 
     public ArmSubsystem() {
-
+        m_dutyCycleRequest = new DutyCycleOut(0.0);
+        m_voltageRequest = new VoltageOut(0.0);
+        m_torqueRequest = new TorqueCurrentFOC(0.0);
+        armMotor = new TalonFX(Constants.Arm.ARM_MOTOR_ID);
+        rollerMotor = new TalonFX(Constants.Arm.ROLLER_MOTOR_ID);
     }
     
 
